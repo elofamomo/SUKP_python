0bf399917ecaecfd37474c0f8c2da856d6f52ae5
DungTruong <v.dungtt81@vinrobotics.net>
2025-09-26 08:57:39 +0700

    new dqn100 nn

--- networks/dqn100.py
+++ networks/dqn100.py
@@ -6,16 +6,16 @@ class DeepQlearningNetwork(nn.Module):
     def __init__(self, input_size, output_size):
         super(DeepQlearningNetwork, self).__init__()
         self.network = nn.Sequential(
-            nn.Linear(input_size, 1024),  # First hidden layer
-            nn.LayerNorm(1024),  # Normalize activations
+            nn.Linear(input_size, 2048),  # First hidden layer
+            nn.LayerNorm(2048),  # Normalize activations
             nn.ReLU(),
-            nn.Linear(1024, 512),  # Second hidden layer
+            nn.Linear(2048, 1024),  # Second hidden layer
+            nn.LayerNorm(1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),  # Third hidden layer
             nn.LayerNorm(512),
             nn.ReLU(),
-            nn.Linear(512, 256),  # Third hidden layer
-            nn.LayerNorm(256),
-            nn.ReLU(),
-            nn.Linear(256, 256),  # Fourth hidden layer
+            nn.Linear(512, 256),  # Fourth hidden layer
             nn.LayerNorm(256),
             nn.ReLU(),
             nn.Linear(256, output_size)  # Output: No norm or activation for Q-values
